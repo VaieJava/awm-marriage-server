7871a381c5a0c418c8422e16ba2b31b9661864bf
vaie <vaielx@163.com>
2020-03-15 21:03:14 +0800

    Initial commit

---
+++ src/main/java/com/outdd/marriage/AwmMarriageServerApplication.java
@@ -0,0 +1,13 @@
+package com.outdd.marriage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwmMarriageServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwmMarriageServerApplication.class, args);
+    }
+
+}
