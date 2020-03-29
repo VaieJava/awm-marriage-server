18ad3167de858a26cbc1fff5d1fedf18d95b93e5
vaie <vaielx@163.com>
2020-03-29 19:53:22 +0800

    Schema

--- src/main/java/com/outdd/marriage/MarriageApplication.java
+++ src/main/java/com/outdd/marriage/MarriageApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AwmMarriageServerApplication {
+public class MarriageApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AwmMarriageServerApplication.class, args);
+        SpringApplication.run(MarriageApplication.class, args);
     }
 
 }
